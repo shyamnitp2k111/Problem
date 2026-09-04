d344ce3a7f0403e4b87778f4ea7610bb249c72a9
Shyam Kishor <shyamnitp2k11@gmail.com>
2026-09-04 10:35:35 +0530

    commit jenkins file

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,6 +9,7 @@ public class Calculator {
     public int sub(int a, int b) {
         return a - b;
     }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
